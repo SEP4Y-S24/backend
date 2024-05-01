2005f79538160e951ac7d717bfc33ab97975aa14
D1e2N3i4M52003 <denim2003@abv.bg>
2024-05-01 12:24:53 +0200

    Added a Web Api project that has a working container

--- Backend/WebApi/Controllers/ClockController.cs
+++ Backend/WebApi/Controllers/ClockController.cs
@@ -4,7 +4,30 @@ namespace WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ClockController
+public class ClockController : ControllerBase
 {
-    
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<ClockController> _logger;
+
+    public ClockController(ILogger<ClockController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
 }
