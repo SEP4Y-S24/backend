ba9c91a3562d7051044ede7db5b28fe6c8418c9e
Nural Hasan <nuralh777@gmail.com>
2024-05-07 12:16:36 +0200

    added classes for user

---
+++ Backend/EfcDatabase/DAOImplementation/UserDAO.cs
@@ -0,0 +1,25 @@
+﻿using Application.DAO;
+using EfcDatabase.Context;
+using EfcDatabase.Model;
+
+namespace Services.Services;
+
+public class UserDAO: IUserDAO
+{
+    private readonly UserContext context;
+
+    public UserDAO(UserContext dbContext)
+    {
+        context = dbContext;
+    }
+
+    public Task<User?> GetByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Clock> CreateAsync(Clock clock)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Backend/Services/Services/ClockService.cs
+++ Backend/Services/Services/ClockService.cs
@@ -7,10 +7,12 @@ namespace Services.Services;
 public class ClockService : IClockService
 {
     private readonly IClockDAO _clockDao;
+    private readonly IUserDAO _userDao;
 
-    public ClockService(IClockDAO clockDao)
+    public ClockService(IClockDAO clockDao, IUserDAO userDao)
     {
         this._clockDao = clockDao;
+        _userDao = userDao;
     }
 
     public async Task SetTimeZoneAsync(char TimeZone, Guid id)
@@ -38,5 +40,23 @@ public class ClockService : IClockService
 
         await _clockDao.UpdateAsync(existing);
 
+    }
+
+    public async Task<Clock> CreateClockAsync(Clock clockToCreate)
+    {
+        User? user = await _userDao.GetByIdAsync(clockToCreate.OwnerId);
+        if (user == null)
+        {
+            throw new Exception($"User with id {clockToCreate.OwnerId} was not found.");
+        }
+
+        //ValidateTodo(dto);
+        // Post post = new Post(user.Id, dto.Title, dto.Content);
+        // Post created = await postDao.CreateAsync(post);
+        // return created;
+
+        Clock clock = new Clock(user, clockToCreate.Name, clockToCreate.TimeZone);
+        Clock created = await _userDao.CreateAsync(clock);
+        return created;
     }
 }
