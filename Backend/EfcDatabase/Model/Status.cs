459db31b5cf37efaa92cc6d319f10b9eb7c73e06
Nural Hasan <nuralh777@gmail.com>
2024-05-08 10:08:21 +0200

    task classes created

---
+++ Backend/EfcDatabase/Model/Status.cs
@@ -0,0 +1,6 @@
+﻿namespace EfcDatabase.Model;
+
+public enum Status
+{
+    Started, InProgress, Finished
+}
---
+++ Backend/EfcDatabase/Model/Task.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EfcDatabase.Model;
+
+public class Task
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public Status Status { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+
+    public Task(User user, string name, string description, DateTime deadline, Status status)
+    {
+        User = user;
+        UserId = user.Id;
+        Name = name;
+        Description = description;
+        Deadline = deadline;
+        Status = status;
+    }
+    
+
+}
