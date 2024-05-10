44391826987490dca7058e20fb95c8d37b72c4a8
Nural Hasan <nuralh777@gmail.com>
2024-05-10 11:48:54 +0200

    alarm dao and service classes

---
+++ Backend/EfcDatabase/IDAO/IAlarmDAO.cs
@@ -0,0 +1,12 @@
+﻿using EfcDatabase.Model;
+
+namespace EfcDatabase.IDAO;
+
+public interface IAlarmDAO
+{
+    Task<Alarm> CreateAsync(Alarm alarm);
+    Task UpdateAsync(Alarm alarm);
+    Task<Alarm?> GetByIdAsync(Guid alarmId);
+    Task DeleteAsync(Guid alarmId);
+    Task<IEnumerable<Alarm>> GetAllAsync();
+}
---
+++ Backend/Services/Services/AlarmService.cs
@@ -0,0 +1,69 @@
+﻿using EfcDatabase.IDAO;
+using EfcDatabase.Model;
+using Services.IServices;
+
+namespace Services.Services;
+
+public class AlarmService:IAlarmService
+{
+    private readonly IAlarmDAO _alarmDao;
+
+    public AlarmService(IAlarmDAO alarmDao)
+    {
+        _alarmDao = alarmDao;
+    }
+
+
+    public async Task<Alarm> CreateAsync(Alarm alarmToCreate)
+    {
+        try
+        {
+            Alarm alarm = await _alarmDao.CreateAsync(alarmToCreate);
+            return alarm;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public async Task<Alarm?> GetByIdAsync(Guid alarmId)
+    {
+        try
+        {
+            Alarm? alarm = await _alarmDao.GetByIdAsync(alarmId);
+            return alarm;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw new InvalidOperationException();
+        }
+    }
+
+    public async Task UpdateAsync(Alarm alarm)
+    {
+        try
+        {
+            await _alarmDao.UpdateAsync(alarm);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task DeleteAsync(Guid alarmId)
+    {
+        try
+        {
+            await _alarmDao.DeleteAsync(alarmId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+}
