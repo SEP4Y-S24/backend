75a1b5ffa9ac751573e95a776827d14f9dc2b84e
Cosmin Gabriel Demian <cosmindemian49@gmail.com>
2024-05-17 10:53:05 +0200

    Model and DbContext added
    Model for AlarmService and DbContext added to project.

---
+++ Backend/AAlarmService/Context/AlarmContext.cs
@@ -0,0 +1,55 @@
+﻿using AlarmServices.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlarmServices.Context;
+
+public class AlarmContext: DbContext
+{
+    public AlarmContext() { }
+    public AlarmContext(DbContextOptions<AlarmContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=clockcontext.postgres.database.azure.com;" +
+            "Port=5432;" +
+            "Database=AlarmService;" +
+            "Username=clockcontext;" +
+            "Password=postgres123!;");
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Clock> Clocks { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Alarm> Alarms { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = Guid.Parse("5f3bb5af-e982-4a8b-8590-b620597a7360"),
+            });
+        modelBuilder.Entity<Clock>().HasData(
+            new Clock
+            {
+                Id = Guid.Parse("f656d97d-63b7-451a-91ee-0e620e652c9e"),
+                OwnerId = Guid.Parse("5f3bb5af-e982-4a8b-8590-b620597a7360"),
+                Name = "Test Clock",
+                TimeOffset = 0,
+            });
+        modelBuilder.Entity<Alarm>().HasData(
+            new Alarm
+            {
+                Id = Guid.Parse("f656d97d-63b7-451a-91ee-0e620e652c9e"),
+                ClockId = Guid.Parse("f656d97d-63b7-451a-91ee-0e620e652c9e"),
+                IsSnoozed = false,
+                SetOffTime = DateTime.UtcNow.AddHours(1), // SetOffTime (example: 1 hour from now)
+                IsActive = true // IsActive
+            });
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Backend/AAlarmService/Model/User.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AlarmServices.Model;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set;}
+
+}
