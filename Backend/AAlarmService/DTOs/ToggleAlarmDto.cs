9554129c1b6d364043cecf2d122cbdbd9a8f3a0e
D1e2N3i4M52003 <denim2003@abv.bg>
2024-05-20 11:42:47 +0200

    ToggleAlarm endPoint created

---
+++ Backend/AAlarmService/DTOs/ToggleAlarmDto.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace AAlarmServices.DTOs;
+
+public class ToggleAlarmDto
+{
+    [JsonProperty("state")]
+    public bool state { get; set; }
+}
--- Backend/AAlarmService/Functions/AlarmServiceFunctions.cs
+++ Backend/AAlarmService/Functions/AlarmServiceFunctions.cs
@@ -93,5 +93,26 @@ namespace CoupledClock.AlarmService
             return new OkObjectResult(alarmDto);        
         }
 
+        [Function("ToggleAlarm")]
+        public async Task<IActionResult> ToggleAlarm(
+            [HttpTrigger(AuthorizationLevel.Function, "patch", Route = "alarm/{alarmId}/state")] HttpRequest req,
+            Guid alarmId)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            ToggleAlarmDto state = JsonConvert.DeserializeObject<ToggleAlarmDto>(requestBody);
+            var alarmService = ServiceFactory.GetAlarmService();
+            Alarm alarm = await alarmService.ToggleAlarmAsync(alarmId, state.state);
+            AlarmDTO alarmDto = new AlarmDTO
+            {
+                Id = alarm.Id,
+                ClockId = alarm.ClockId,
+                SetOffTime = alarm.SetOffTime,
+                IsActive = alarm.IsActive,
+                IsSnoozed = alarm.IsSnoozed
+            };
+            return new OkObjectResult(alarmDto);
+        }
+
     }
 }
