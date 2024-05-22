170776468de249c6e3e263fece62ff2a92e2d4ac
D1e2N3i4M52003 <denim2003@abv.bg>
2024-05-22 11:45:08 +0200

    Refactored AAlarm

--- Backend/AAlarmService/DTOs/AlarmsDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿using AAlarmService.Model;
-
-namespace AAlarmService.DTOs;
-
-public class AlarmsDTO
-{
-    public ICollection<AlarmDTO> Alarms { get; set; }
-}
--- Backend/AAlarmService/DTOs/ToggleAlarmDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace AAlarmService.DTOs;
-
-public class ToggleAlarmDto
-{
-    [JsonProperty("state")]
-    public bool? state { get; set; }
-}
