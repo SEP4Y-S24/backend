1945c112a9994244ccb8b2c02c2d5fd30d43a460
D1e2N3i4M52003 <denim2003@abv.bg>
2024-05-24 10:41:06 +0200

    Deploy AEventService to Azure

---
+++ Backend/AEventService/Functions/ClockService.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+
+namespace AEventService.Functions
+{
+    public class ClockService
+    {
+        private readonly ILogger<ClockService> _logger;
+
+        public ClockService(ILogger<ClockService> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("ClockService")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        
+    }
+}
